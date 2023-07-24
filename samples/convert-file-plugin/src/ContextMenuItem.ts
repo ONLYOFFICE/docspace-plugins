c3c86d771b8b8b5b65a559ec24a2376d4ac5e6f6
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-07-24 10:38:21 +0300

    Samples: ConverFilePlugin: update version, context menu item

--- samples/convert-file-plugin/src/ContextMenuItem.ts
+++ samples/convert-file-plugin/src/ContextMenuItem.ts
@@ -1,15 +1,15 @@
 import {
-  ContextMenuItemType,
   FilesExst,
+  FilesType,
   IContextMenuItem,
 } from "@onlyoffice/docspace-plugin-sdk";
 
 export const convertFileItem: IContextMenuItem = {
   key: "convert-file-item",
-  type: ContextMenuItemType.Files,
   position: 0,
   label: "Convert file",
   icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF5z5FvWU-DjRHm8ankd_VwHEgpSSGRhVUiA&usqp=CAU",
   onClick: () => {},
+  fileType: [FilesType.file],
   fileExt: [FilesExst.docx, FilesExst.xlsx],
 };
